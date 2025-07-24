3b242cd65e9707524e4b43b7ea50298cb0231773
Peterhzx <peterzixuanhe@gmail.com>
2025-07-24 22:56:26 +0800

    fix: read config.json with encoding='utf-8'

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ from src.tokenizer import Tokenizer
 
 
 def load_hyperparam(json_path=r".\config\config.json"):
-    with open(json_path, 'r') as f:
+    with open(json_path, 'r', encoding='utf-8') as f:
         params = json.load(f)
     with open(r".\config\schema.json", 'r') as f:
         schema = json.load(f)
@@ -24,7 +24,7 @@ def train_tokenizer(params):
     tokenizer.save()
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # .\\config\\config.json
     if len(sys.argv) < 2:
         print("Usage: python train.py <config_path>")
         sys.exit(1)
