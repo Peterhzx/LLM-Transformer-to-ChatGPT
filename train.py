5250dd1b03957d7a7cdf40351f4670fdd76a0b14
Peterhzx <peterzixuanhe@gmail.com>
2025-07-24 21:18:41 +0800

    chore: fill config.json and schema.json

--- train.py
+++ train.py
@@ -3,14 +3,14 @@ import json
 from jsonschema import validate
 
 
-def load_hyperparam(json_path="./param.json"):
+def load_hyperparam(json_path=r".\param.json"):
     with open(json_path, 'r') as f:
         params = json.load(f)
-    with open("./config/schema.json", 'r') as f:
+    with open(r".\config\schema.json", 'r') as f:
         schema = json.load(f)
     validate(instance=params, schema=schema)
     return params
 
 
 if __name__ == '__main__':
-    hyperparams = load_hyperparam("./config/config.json")
+    hyperparams = load_hyperparam(r".\config\config.json")
