219531ea94d121d945754eb68a38020957ae2a5c
Peterhzx <peterzixuanhe@gmail.com>
2025-09-09 11:43:54 +0800

    Revert "perf: move preprocessing of tokenized data out of datasets.py(__getitem__) to dataloader.py"
    This reverts commit 52afbec9db8c848254c4e5f799fb7ca29f5da8ad.

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,10 +1,7 @@
-import ast
 import os
-import random
 
 import pandas as pd
-import torch
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
 from src.datasets import TransformerDataset, BERTDataset
@@ -68,34 +65,7 @@ class Dataloader:
     def get_df(self, sample_size=0.1):
         return self.df.sample(frac=sample_size)
 
-    def _postprocess_tokenized_data_for_transformer(self, max_len, pad_token_id, bos_token_id, eos_token_id):
-        column_list = self.df.columns.tolist()
-        self.df.rename(columns={column_list[0]: "src_input", column_list[1]: "decoder_input"}, inplace=True)
-        self.df["target"] = self.df["decoder_input"]
-        for idx in tqdm(range(len(self.df)), desc="Postprocessing tokenized data", total=len(self.df)):
-            src_tokenized = self.df.at[idx, "src_input"]
-            tgt_tokenized = self.df.at[idx, "decoder_input"]
-
-            # prune and padding
-            src_input = src_tokenized[:max_len]
-            src_padding = [pad_token_id] * (max_len - len(src_input))
-            src_input = src_input + src_padding
-
-            tgt_input = tgt_tokenized[:max_len - 1]
-
-            decoder_input = [bos_token_id] + tgt_input
-            decoder_input += [pad_token_id] * (max_len - len(decoder_input))
-
-            target = tgt_input + [eos_token_id]
-            target += [pad_token_id] * (max_len - len(target))
-
-            self.df.at[idx, "src_input"] = src_input
-            self.df.at[idx, "decoder_input"] = decoder_input
-            self.df.at[idx, "target"] = target
-
     def get_transformer_dataloader(self, max_seq_len, batch_size, num_workers, pin_memory, pad_token_id=0, bos_token_id=1, eos_token_id=2, train_val_test_split=None):
-        self._postprocess_tokenized_data_for_transformer(max_seq_len, pad_token_id, bos_token_id, eos_token_id)
-
         if train_val_test_split is None:
             train_val_test_split = [0.7, 0.15, 0.15]
 
@@ -103,9 +73,9 @@ class Dataloader:
         train_size = int(train_val_test_split[0] * dataset_size)
         val_size = int(train_val_test_split[1] * dataset_size)
 
-        dataset = TransformerDataset(self.df)
-
-        train_set, val_set, test_set = random_split(dataset, [train_size, val_size, dataset_size - train_size - val_size], generator=torch.Generator().manual_seed(42))
+        train_set = TransformerDataset(self.df.iloc[:train_size], max_seq_len, pad_token_id, bos_token_id, eos_token_id)
+        val_set = TransformerDataset(self.df.iloc[train_size:(train_size + val_size)], max_seq_len, pad_token_id, bos_token_id, eos_token_id)
+        test_set = TransformerDataset(self.df.iloc[(train_size + val_size):dataset_size], max_seq_len, pad_token_id, bos_token_id, eos_token_id)
 
         train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
         val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
@@ -113,64 +83,7 @@ class Dataloader:
 
         return train_loader, val_loader, test_loader
 
-    def _postprocess_tokenized_data_for_bert(self, max_len, random_token_start, random_token_end, pad_token_id, mask_token_id, cls_token_id, sep_token_id):
-        column_list = self.df.columns.tolist()
-        self.df.rename(columns={column_list[0]: "source", column_list[1]: "target"}, inplace=True)
-        self.df["mask"] = self.df["target"]
-        for idx in tqdm(range(len(self.df)), desc="Postprocessing tokenized data", total=len(self.df)):
-            a_tokenized = self.df.at[idx, "source"]
-            if random.random() > 0.5:
-                b_tokenized = self.df.at[idx, "target"]
-                isnext_token = 1
-            else:
-                i = random.randint(0, len(self.df) - 2)
-                b_tokenized = self.df.at[i + 1 if i >= idx else i, "target"]
-                isnext_token = 0
-
-            # create tgt seq
-            tgt_tokenized = [isnext_token] + a_tokenized + [sep_token_id] + b_tokenized
-            target = tgt_tokenized[:max_len]
-            target += [pad_token_id] * (max_len - len(target))
-
-            # mask src
-            length = len(a_tokenized) + len(b_tokenized)
-            if length > max_len - 2:
-                length = max_len - 2
-            pred_list = random.sample(range(length), int(length * 0.15) if int(length * 0.15) > 0 else 1)
-
-            for i in range(len(pred_list)):
-                if pred_list[i] >= len(a_tokenized):
-                    pred_list[i] += 2
-                else:
-                    pred_list[i] += 1
-
-            src_tokenized = [cls_token_id] + a_tokenized + [sep_token_id] + b_tokenized
-
-            mask_list = random.sample(pred_list, int(len(pred_list) * 0.9) if int(len(pred_list) * 0.9) > 0 else 1)
-            for i in mask_list:
-                src_tokenized[i] = mask_token_id
-
-            random_list = random.sample(mask_list, int(len(mask_list) * (1 / 9)) if int(len(mask_list) * (1 / 9)) > 0 else 1)
-            for i in random_list:
-                src_tokenized[i] = random.randint(random_token_start, random_token_end)
-
-            source = src_tokenized[:max_len]
-            source += [pad_token_id] * (max_len - len(source))
-
-            mask = []
-            for i in range(len(source)):
-                if i in pred_list:
-                    mask.append(True)
-                else:
-                    mask.append(False)
-
-            self.df.at[idx, "source"] = source
-            self.df.at[idx, "target"] = target
-            self.df.at[idx, "mask"] = mask
-
     def get_bert_dataloader(self, max_seq_len, batch_size, random_token_start, random_token_end, num_workers, pin_memory, pad_token_id=0, mask_token_id=1, cls_token_id=2, sep_token_id=3, train_val_test_split=None):
-        self._postprocess_tokenized_data_for_bert(max_seq_len, random_token_start, random_token_end, pad_token_id, mask_token_id, cls_token_id, sep_token_id)
-
         if train_val_test_split is None:
             train_val_test_split = [0.7, 0.15, 0.15]
 
@@ -178,9 +91,9 @@ class Dataloader:
         train_size = int(train_val_test_split[0] * dataset_size)
         val_size = int(train_val_test_split[1] * dataset_size)
 
-        dataset = BERTDataset(self.df)
-
-        train_set, val_set, test_set = random_split(dataset, [train_size, val_size, dataset_size - train_size - val_size], generator=torch.Generator().manual_seed(42))
+        train_set = BERTDataset(self.df.iloc[:train_size], max_seq_len, random_token_start, random_token_end, pad_token_id, mask_token_id, cls_token_id, sep_token_id)
+        val_set = BERTDataset(self.df.iloc[train_size:(train_size + val_size)], max_seq_len, random_token_start, random_token_end, pad_token_id, mask_token_id, cls_token_id, sep_token_id)
+        test_set = BERTDataset(self.df.iloc[(train_size + val_size):dataset_size], max_seq_len, random_token_start, random_token_end, pad_token_id, mask_token_id, cls_token_id, sep_token_id)
 
         train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
         val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory)
--- src/datasets.py
+++ src/datasets.py
@@ -1,34 +1,113 @@
+import ast
+import random
+
 import torch
 from torch.utils.data import Dataset
 
 
 class TransformerDataset(Dataset):
-    def __init__(self, df):
+    def __init__(self, df, max_len, pad_token_id=0, bos_token_id=1, eos_token_id=2):
         super(TransformerDataset, self).__init__()
         self.df = df
-
-    def __len__(self):
-        return len(self.df)
-
-    def __getitem__(self, idx):
-        src_tensor = torch.tensor(self.df.at[idx, "src_input"], dtype=torch.long)
-        decoder_input_tensor = torch.tensor(self.df.at[idx, "decoder_input"], dtype=torch.long)
-        target_tensor = torch.tensor(self.df.at[idx, "target"], dtype=torch.long)
-
-        return src_tensor, decoder_input_tensor, target_tensor
-
-
-class BERTDataset(Dataset):
-    def __init__(self, df):
-        super(BERTDataset, self).__init__()
-        self.df = df
+        self.max_len = max_len
+        self.pad_token_id = pad_token_id
+        self.bos_token_id = bos_token_id
+        self.eos_token_id = eos_token_id
 
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):  # improvement: Instead of fixed-length sequences, consider dynamic batching with a custom collate_fn
-        source_tensor = torch.tensor(self.df.at[idx, "source"], dtype=torch.long)
-        target_tensor = torch.tensor(self.df.at[idx, "target"], dtype=torch.long)
-        mask_tensor = torch.tensor(self.df.at[idx, "mask"], dtype=torch.long)
+        row = self.df.iloc[idx]
+        col_names = self.df.columns.tolist()
+        src_tokenized = row[col_names[0]]
+        tgt_tokenized = row[col_names[1]]
 
-        return source_tensor, target_tensor, mask_tensor
+        if isinstance(src_tokenized, str) or isinstance(tgt_tokenized, str):
+            src_tokenized = ast.literal_eval(src_tokenized)
+            tgt_tokenized = ast.literal_eval(tgt_tokenized)
+
+        # prune and padding
+        src_input = src_tokenized[:self.max_len]
+        src_padding = [self.pad_token_id] * (self.max_len - len(src_input))
+        src_input = src_input + src_padding
+
+        tgt_input = tgt_tokenized[:self.max_len - 1]
+
+        decoder_input = [self.bos_token_id] + tgt_input
+        decoder_input += [self.pad_token_id] * (self.max_len - len(decoder_input))
+
+        target = tgt_input + [self.eos_token_id]
+        target += [self.pad_token_id] * (self.max_len - len(target))
+
+        src_tensor = torch.tensor(src_input, dtype=torch.long)
+        decoder_input_tensor = torch.tensor(decoder_input, dtype=torch.long)
+        target_tensor = torch.tensor(target, dtype=torch.long)
+
+        return src_tensor, decoder_input_tensor, target_tensor
+
+
+class BERTDataset(Dataset):
+    def __init__(self, df, max_len, random_token_start, random_token_end, pad_token_id=0, mask_token_id=1, cls_token_id=2, sep_token_id=3):
+        super(BERTDataset, self).__init__()
+        self.df = df
+        self.max_len = max_len
+        self.random_token_start = random_token_start
+        self.random_token_end = random_token_end
+        self.pad_token_id = pad_token_id
+        self.mask_token_id = mask_token_id
+        self.cls_token_id = cls_token_id
+        self.sep_token_id = sep_token_id
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):  # improvement: Instead of fixed-length sequences, consider dynamic batching with a custom collate_fn
+        a_tokenized = self.df.iloc[idx, 0]
+        if random.random() > 0.5:
+            b_tokenized = self.df.iloc[idx, 1]
+            isnext_token = 1
+        else:
+            i = random.randint(0, len(self.df)-2)
+            b_tokenized = self.df.iloc[i + 1 if i >= idx else i, 1]
+            isnext_token = 0
+
+        # create tgt seq
+        tgt_tokenized = [isnext_token] + a_tokenized + [self.sep_token_id] + b_tokenized
+        target = tgt_tokenized[:self.max_len - 1]
+        target += [self.pad_token_id] * (self.max_len - len(target))
+
+        # mask src
+        length = len(a_tokenized) + len(b_tokenized)
+        if length > self.max_len - 2:
+            length = self.max_len - 2
+        pred_list = random.sample(range(length), int(length * 0.15) if int(length * 0.15) > 0 else 1)
+
+        for i in range(len(pred_list)):
+            if pred_list[i] >= len(a_tokenized):
+                pred_list[i] += 2
+            else:
+                pred_list[i] += 1
+
+        src_tokenized = [isnext_token] + a_tokenized + [self.sep_token_id] + b_tokenized
+
+        mask_list = random.sample(pred_list, int(len(pred_list) * 0.9) if int(len(pred_list) * 0.9) > 0 else 1)
+        for i in mask_list:
+            src_tokenized[i] = self.mask_token_id
+
+        random_list = random.sample(mask_list, int(len(mask_list) * (1 / 9)) if int(len(mask_list) * (1 / 9)) > 0 else 1)
+        for i in random_list:
+            src_tokenized[i] = random.randint(self.random_token_start, self.random_token_end)
+
+        source = src_tokenized[:self.max_len - 1]
+        source += [self.pad_token_id] * (self.max_len - len(source))
+
+        pred_list = pred_list[:self.max_len - 1]
+        pred_list += [self.pad_token_id] * (self.max_len - len(pred_list))
+
+        # to tensor
+        src_tensor = torch.tensor(source, dtype=torch.long)
+        tgt_tensor = torch.tensor(target, dtype=torch.long)
+        pred_tensor = torch.tensor(pred_list, dtype=torch.long)
+
+        return src_tensor, tgt_tensor, pred_tensor
